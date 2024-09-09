5a00806ffbe0999f18df32ccd6a9ba907d6ea985
devopsandes <devops@andessalud.ar>
2024-09-09 15:46:13 -0300

    add Auth decorator

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,10 +3,12 @@ import { AuthService } from './auth.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { LoginUserDto } from './dto/login-user.dto';
 import { AuthGuard } from '@nestjs/passport';
-import { GetUser, RawHeaders } from './decorators';
+import { Auth, GetUser, RawHeaders } from './decorators';
 import { User } from './entities/user.entity';
 import { IncomingHttpHeaders } from 'http';
 import { UserRoleGuard } from './guards/user-role/user-role.guard';
+import { RoleProtected } from './decorators/role-protected.decorator';
+import { ValidRoles } from './interfaces';
 
 @Controller('auth')
 export class AuthController {
@@ -40,13 +42,26 @@ export class AuthController {
       headers
     }
   }
+  // @SetMetadata('roles',['admin','super-user'])
 
   @Get('private2')
-  @SetMetadata('roles',['admin','super-user'])
-  @UseGuards(AuthGuard(), UserRoleGuard )
+  @RoleProtected(ValidRoles.superUser,ValidRoles.admin)
+  @UseGuards(AuthGuard(), UserRoleGuard)
   privateRoute2(
     @GetUser() user: User
   ){
+
+    return {
+      ok: true,
+      user
+    }
+  }
+
+  @Get('private3')
+  @Auth(ValidRoles.admin)
+  privateRoute3(
+    @GetUser() user: User
+  ) {
 
     return {
       ok: true,
--- src/auth/guards/user-role/user-role.guard.ts
+++ src/auth/guards/user-role/user-role.guard.ts
@@ -1,6 +1,8 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { BadRequestException, CanActivate, ExecutionContext, ForbiddenException, Injectable } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { Observable } from 'rxjs';
+import { User } from 'src/auth/entities/user.entity';
+import { META_ROLES } from 'src/auth/decorators/role-protected.decorator';
 
 @Injectable()
 export class UserRoleGuard implements CanActivate {
@@ -13,10 +15,25 @@ export class UserRoleGuard implements CanActivate {
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
 
-    const validRoles: string[] = this.reflector.get('roles',context.getHandler())
-    console.log({validRoles});
-    
+    const validRoles: string[] = this.reflector.get(META_ROLES,context.getHandler())
 
-    return true;
+    if(!validRoles) return true
+    if(validRoles.length === 0) return true
+
+    const req = context.switchToHttp().getRequest()
+    const user = req.user as User
+
+    if(!user)
+      throw new BadRequestException('User not found')
+
+
+    for (const role of user.roles) {
+      if(validRoles.includes(role)){
+        return true
+      }
+    }
+    
+    throw new ForbiddenException(`User ${user.fullName} needs a valid role: [${validRoles}]`)
+
   }
 }
